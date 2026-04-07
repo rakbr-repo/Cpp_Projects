8c5df53f16eb70b0d0432b9287b79c79bec6031b
rakbr-repo <brrakshith841@gmail.com>
2026-04-07 22:58:17 +0530

    Makefile understanding and driver init exit memory optimization

--- First_Loadable_Driver/driver_load.c
+++ First_Loadable_Driver/driver_load.c
@@ -1,20 +1,20 @@
 #include<linux/init.h>
 #include<linux/module.h>
 
-MODULE_LICENSE("GPL")
-MODULE_AUTHOR("Rakshith Loadable driver")
-MODULE_DESCRIPTION("This is a loadable driver")
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Rakshith Loadable driver");
+MODULE_DESCRIPTION("This is a loadable driver");
 
-static int c_tor(void)
+static int _init my_init(void)
 {
-    printk("Creating loadable driver")
+    printk("Creating loadable driver\n");
     return 0;
 }
 
-static void d_tor(void)
+static void _exit my_exit(void)
 {
-    printk("Destroy loadable driver");
+    printk("Destroy loadable driver\n");
 }
 
-module_init(c_tor);
-module_exit(d-tor);
+module_init(my_init);
+module_exit(my_exit);
